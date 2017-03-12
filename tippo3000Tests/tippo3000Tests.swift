14eeeb5ad5281df4cb1e5d5cfb303c897f1de259
Arvy Gill <arvy_gill@intuit.com>
2017-03-12 08:20:32 -0700

    Initial Commit

---
+++ tippo3000Tests/tippo3000Tests.swift
@@ -0,0 +1,36 @@
+//
+//  tippo3000Tests.swift
+//  tippo3000Tests
+//
+//  Created by Gill, Arvy on 3/12/17.
+//  Copyright © 2017 Gill, Arvy. All rights reserved.
+//
+
+import XCTest
+@testable import tippo3000
+
+class tippo3000Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
